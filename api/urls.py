74e23d0f314fbdd016df73a56fc1b0cf208b0384
AkhAkhmad <am9797920@icloud.com>
2023-10-25 11:23:41 +0300

    fix urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import MasterViewSet, SalonViewSet
 
 urlpatterns = [
-    path('salon/', SalonViewSet.as_view({'get': 'list'})),
-    path('master/', MasterViewSet.as_view({'get': 'list'})),
+    path('salon/', SalonViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('master/', MasterViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('salon/<int:pk>/', SalonViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('master/<int:pk>/', MasterViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
