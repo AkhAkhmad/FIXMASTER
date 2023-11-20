9ebd773b8b7b8965f03f9ab73e15afacb58579e2
AkhAkhmad <am9797920@icloud.com>
2023-11-20 11:04:52 +0300

    fix

---
+++ api/receivers.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from . import models
+
+
+@receiver(post_save, sender=models.Order)
+def create_booking(sender, created, instance, **kwargs):
+    if created:
+        models.Booking.objects.create(booking_date=instance.begin_date, booking_time=instance.begin_time,
+                                      master=instance.master)
+    
